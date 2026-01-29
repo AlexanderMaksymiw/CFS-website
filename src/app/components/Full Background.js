bbb01c7bff77ef19637a30a7181a4669f6dc86fb
AlexanderMaksymiw <amaksymiw02@gmail.com>
2026-01-29 18:46:50 +0000

    home page background update

--- src/app/components/Full Background.js
+++ src/app/components/Full Background.js
@@ -3,15 +3,9 @@
 export default function FullBackground({ children }) {
   return (
     <div className="relative h-162.5 md:h-212.5 bg-center bg-fixed overflow-hidden">
-      <video
+      <Image
         className="absolute inset-0 w-full h-full object-cover z-0"
-        src="/videos/cfs-video.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-        poster="/images/hero/cfs-poster.jpg"
-
+        src="/images/hero/cfs-background.jpg"
       />
 
       <div className=" pt-25 flex relative z-20">
